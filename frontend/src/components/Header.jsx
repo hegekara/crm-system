5f8fedda448189c9e8e7eac5148a1b3db0e60de8
Hilmi Ege Kara <hegekara48@gmail.com>
2025-08-01 13:20:56 +0300

    added header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+            <div className="container-fluid">
+                <a className="navbar-brand">CRM Sistemi</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+                    <ul className="navbar-nav flex-grow-1">
+                        <li className="nav-item">
+                            <a className="nav-link text-dark" href="/customers">Müşteriler</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-dark" href="/users">Kullanıcılar</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
