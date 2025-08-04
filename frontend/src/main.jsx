62ef13c08df3f3849526b4077917c1840548491d
Hilmi Ege Kara <hegekara48@gmail.com>
2025-08-04 14:33:21 +0300

    added protected route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import Home from './pages/Home';
 import CustomerList from './pages/Customer/CustomerList';
 import CreateCustomer from './pages/Customer/CustomerCreate';
+import Login from './pages/Auth/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 createRoot(document.getElementById('root')).render(
-  <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Home />} />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
 
-        <Route path="/customers" element={<CustomerList />} />
-        <Route path="/create-customer" element={<CreateCustomer />} />
-      </Routes>
-    </BrowserRouter>
-  </>
-)
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      } />
+      <Route path="/home" element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      } />
+      <Route path="/customers" element={
+        <ProtectedRoute>
+          <CustomerList />
+        </ProtectedRoute>
+      } />
+      <Route path="/create-customer" element={
+        <ProtectedRoute>
+          <CreateCustomer />
+        </ProtectedRoute>
+      } />
+    </Routes>
+  </BrowserRouter>
+);
