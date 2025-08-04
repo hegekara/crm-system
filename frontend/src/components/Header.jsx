81ccf6a39aed1cc354b23300b01048f65799fe30
Hilmi Ege Kara <hegekara48@gmail.com>
2025-08-04 15:22:24 +0300

    added logout button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-
 const Header = () => {
+    const handleLogout = () => {
+        localStorage.clear();
+        window.location.reload();
+    };
+
     return (
         <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
             <div className="container-fluid">
                 <a className="navbar-brand" href='/home'>CRM Sistemi</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
+                    aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+                <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between w-100">
                     <ul className="navbar-nav flex-grow-1">
                         <li className="nav-item">
                             <a className="nav-link text-dark" href="/customers">Müşteriler</a>
@@ -22,6 +26,9 @@ const Header = () => {
                             <a className="nav-link text-dark" href="/users">Kullanıcılar</a>
                         </li>
                     </ul>
+                    <button className="btn btn-outline-danger" onClick={handleLogout}>
+                        Çıkış Yap
+                    </button>
                 </div>
             </div>
         </nav>
